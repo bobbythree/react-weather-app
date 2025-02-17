cedf957ea061def0a53ca08cb0ba557b1ae59504
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-17 12:35:15 -0600

    search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import MainCard from "./components/MainCard"
+import Search from "./components/Search"
 
 export default function App() {
   return (
-    <div className='grid place-items-center h-screen bg-cyan-500'>
+    <div className='flex flex-col gap-1 justify-center h-screen bg-cyan-500'>
       <MainCard />
+      <Search />
     </div>
   )
 }
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,6 +1,6 @@
 export default function MainCard() {
   return (
-    <div className="bg-cyan-600 flex flex-col rounded-md text-center px-5">
+    <div className="bg-cyan-600 place-self-center flex flex-col rounded-md text-center px-5">
       <h1 className="pt-5 text-5xl">Chicago</h1>
       <img className=" pt-3 place-self-center" src="src/assets/react.svg" alt="logo" style={{width: '50px'}} />
       <h3 className="pt-3 text-xl">sunny/clear</h3>
