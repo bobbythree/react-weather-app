890da61404a5e309364b2e23a5ce97369aae9fd4
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-17 14:02:13 -0600

    basic api call setup, add icons

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,4 +1,30 @@
+import { useState, useEffect } from "react";
+
 export default function MainCard() {
+
+  const [weatherData, setWeatherData] = useState(false);
+
+  const getWeatherByCity = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${import.meta.env.VITE_API_KEY}`
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setWeatherData({
+        city: data.name,
+        temp: Math.floor(data.main.temp),
+        icon: data
+      });
+      
+    } catch (error) {
+      
+    }    
+  }
+
+  useEffect(() => {
+    getWeatherByCity('Chicago')
+  }, []);
+
   return (
     <div className="bg-cyan-600 place-self-center flex flex-col rounded-md text-center px-5">
       <h1 className="pt-5 text-5xl">Chicago</h1>
