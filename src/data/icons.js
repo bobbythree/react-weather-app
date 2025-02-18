e872e24de1900218626ae3fd515e4b95c9fff982
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-17 19:38:34 -0600

    move data to data dir

---
+++ src/data/icons.js
@@ -0,0 +1,28 @@
+import sunny from '../assets/clear_day.png'
+import moon from '../assets/clear_night.png'
+import cloudy from '../assets/cloudy.png'
+import wind from '../assets/wind.png'
+import partlyCloudyDay from '../assets/partly_cloudy_day.png'
+import partlyCloudyNight from '../assets/partly_cloudy_night.png'
+import rain from '../assets/rain.png'
+import snow from '../assets/snow.png'
+import storm from '../assets/storm.png'
+
+export const icons = {
+  "01d": sunny,
+  "01n": moon,
+  "02d": partlyCloudyDay,
+  "02n": partlyCloudyNight,
+  "03d": partlyCloudyDay,
+  "03n": partlyCloudyNight,
+  "04d": cloudy,
+  "04n": cloudy,
+  "09d": rain,
+  "09n": rain,
+  "10d": rain,
+  "10n": rain,
+  "11d": storm,
+  "11n": storm,
+  "13d": snow,
+  "13n": snow
+}
