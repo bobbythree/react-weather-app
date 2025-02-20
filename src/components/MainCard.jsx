ed9a4aaacd052f061e4e2c772836d0571fb007e1
bobbythree <mynameisbobbylewis@hotmail.com>
2025-02-19 19:04:45 -0600

    clear night and day bg

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -48,15 +48,17 @@ export default function MainCard() {
   return (
     <>
     {weatherData ? 
-    <div className={`${isDaytime ? 'bg-cyan-900' : 'bg-black'} flex flex-col justify-center gap-1 h-screen`}>
-      <div className={`${isDaytime ? 'bg-cyan-600' : 'bg-slate-900'} place-self-center flex flex-col rounded-md text-center px-5 min-w-3xs`}>
+    <div className={`${isDaytime ?
+    'bg-[url(./assets/sunny-bg.jpg)] bg-cover' :
+    'bg-[url(./assets/stars-bg.jpg)] bg-cover'} flex flex-col justify-center gap-1 h-screen`}>
+      <div className={`${isDaytime ? 'bg-transparent' : 'bg-black'} place-self-center flex flex-col rounded-md text-center px-5 min-w-3xs`}>
         <h1 className="pt-5 text-5xl">{weatherData.city}</h1>
         <img className=" pt-3 place-self-center" src={weatherData.icon} alt="logo" style={{width: '50px'}} />
         <h3 className="pt-3 text-xl">{weatherData.conditions}</h3>
         <h3 className="py-3 pb-5 text-xl">{`${weatherData.temp}F`}</h3>
       </div>
       <div className="place-self-center">
-        <label className={`${isDaytime ? 'bg-cyan-600' : 'bg-slate-900'} input`}>
+        <label className={`${isDaytime ? 'bg-sky-500' : 'bg-black'} input`}>
           <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g>
           </svg>
           <form onSubmit={handleSubmit}>
